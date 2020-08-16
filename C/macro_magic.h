72c38c64b376eda136186f5fe0da9b0de241b1b0
Jorengarenar <jorengarenar@protonmail.com>
2020-08-16 08:43:33 +0200

    MIN()/MAX() macro

--- C/macro_magic.h
+++ C/macro_magic.h
@@ -4,3 +4,15 @@
 // Remove '\n' after `fgets()`
 #define REMOVE_NL(buffer) \
     buffer[strcspn(buffer, "\n")] = '\0'
+
+// Max of two
+#define MAX(a, b)           \
+    ({ typeof(a) _a = a;    \
+       typeof(b) _b = b;    \
+       _a > _b ? _a : _b; })
+
+// Min of two
+#define MIN(a, b)           \
+    ({ typeof(a) _a = a;    \
+       typeof(b) _b = b;    \
+       _a < _b ? _a : _b; })
